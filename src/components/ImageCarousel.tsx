19f667c4464187186cd7d9e3ae467878cea2624b
Harshit Nathani <natharshit@192.168.1.14>
2025-05-20 03:12:12 +0530

    Describe your changes here

--- src/components/ImageCarousel.tsx
+++ src/components/ImageCarousel.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const ImageCarousel: React.FC = () => {
   const images = [
-    "1.jpeg",
-    "2.jpeg",
-    "3.jpeg",
-    "4.jpeg",
-    "5.jpeg",
+    "/images/1.jpeg",
+    "/images/2.jpeg",
+    "/images/3.jpeg",
+    "/images/4.jpeg",
+    "/images/5.jpeg",
   ];
 
   const [currentIndex, setCurrentIndex] = React.useState(0);
